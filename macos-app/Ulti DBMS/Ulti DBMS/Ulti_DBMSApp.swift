adaae01c903489cd9cbe554f44f39eb7132601a8
adhamelgohary <eaadham347@gmail.com>
2025-12-09 17:05:28 +0200

    chore: Update project dependencies and remove an unused icon.

---
+++ macos-app/Ulti DBMS/Ulti DBMS/Ulti_DBMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ulti_DBMSApp.swift
+//  Ulti DBMS
+//
+//  Created by Adham Elgohary on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Ulti_DBMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
